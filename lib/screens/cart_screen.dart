fb8481806ca343c1ac63460bf8260e2e203eaaf5
Abdul Aliyev <aaa7c4@gmail.com>
2021-01-08 00:28:46 -0500

    Add orders screen

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/cart.dart' show Cart;
 import '../widgets/cart_item.dart';
+import '../providers/orders.dart';
+import './orders_screen.dart';
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';
@@ -36,7 +38,12 @@ class CartScreen extends StatelessWidget {
                     ),
                     FlatButton(
                       child: Text('ORDER NOW'),
-                      onPressed: () {},
+                      onPressed: () {
+                        Provider.of<Orders>(context, listen: false).addOrder(
+                            cart.items.values.toList(), cart.totalAmount);
+                        cart.clear();
+                        Navigator.of(context).pushNamed(OrdersScreen.routeName);
+                      },
                       textColor: Theme.of(context).primaryColor,
                     ),
                   ],
