dfd5f87fc0121a6968bb712a79f64b1eda086a81
Abdul Aliyev <aaa7c4@gmail.com>
2021-01-08 18:55:08 -0500

    Add order items

---
+++ lib/widgets/order_item.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import '../providers/orders.dart' as ord;
+import 'package:intl/intl.dart';
+import 'dart:math';
+
+class OrderItem extends StatefulWidget {
+  final ord.OrderItem order;
+
+  OrderItem(this.order);
+
+  @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expanded = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(10),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text('\$${widget.order.amount}'),
+            subtitle: Text(
+              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
+            ),
+            trailing: IconButton(
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
+            ),
+          ),
+          if (_expanded)
+            Container(
+                height: min(widget.order.products.length * 20.0 + 30, 100),
+                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+                child: ListView(
+                  children: widget.order.products
+                      .map(
+                        (product) => Container(
+                          margin: EdgeInsets.only(bottom: 15),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Text(
+                                product.title,
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                '${product.quantity}x \$${product.price}',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                      .toList(),
+                )),
+        ],
+      ),
+    );
+  }
+}
